c50fce04aa5ad2f377760723460fc3c96a1019e9
Julius <Julius@DEEPTHOUGHT>
2016-06-01 20:57:28 +0200

    Projektdateien hinzufügen.

---
+++ Musicplayer/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Musicplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.listBoxSong = new System.Windows.Forms.ListBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.checkBoxOutput = new System.Windows.Forms.CheckBox();
+            this.checkBoxShuffle = new System.Windows.Forms.CheckBox();
+            this.checkBoxUpload = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(622, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "öffnen";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // listBoxSong
+            // 
+            this.listBoxSong.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxSong.FormattingEnabled = true;
+            this.listBoxSong.Location = new System.Drawing.Point(360, 41);
+            this.listBoxSong.Name = "listBoxSong";
+            this.listBoxSong.Size = new System.Drawing.Size(337, 199);
+            this.listBoxSong.TabIndex = 5;
+            this.listBoxSong.SelectedIndexChanged += new System.EventHandler(this.listBoxSong_SelectedIndexChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "mp3-Dateien|*.mp3|Playlist-Dateien|*.m3u|Alle Dateien|*.*";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(13, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(287, 182);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // checkBoxOutput
+            // 
+            this.checkBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxOutput.AutoSize = true;
+            this.checkBoxOutput.Location = new System.Drawing.Point(12, 200);
+            this.checkBoxOutput.Name = "checkBoxOutput";
+            this.checkBoxOutput.Size = new System.Drawing.Size(111, 17);
+            this.checkBoxOutput.TabIndex = 7;
+            this.checkBoxOutput.Text = "Media Info Output";
+            this.checkBoxOutput.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxShuffle
+            // 
+            this.checkBoxShuffle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxShuffle.AutoSize = true;
+            this.checkBoxShuffle.Location = new System.Drawing.Point(12, 223);
+            this.checkBoxShuffle.Name = "checkBoxShuffle";
+            this.checkBoxShuffle.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxShuffle.TabIndex = 8;
+            this.checkBoxShuffle.Text = "Shuffle";
+            this.checkBoxShuffle.UseVisualStyleBackColor = true;
+            this.checkBoxShuffle.CheckedChanged += new System.EventHandler(this.checkBoxShuffle_CheckedChanged);
+            // 
+            // checkBoxUpload
+            // 
+            this.checkBoxUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxUpload.AutoSize = true;
+            this.checkBoxUpload.Location = new System.Drawing.Point(129, 200);
+            this.checkBoxUpload.Name = "checkBoxUpload";
+            this.checkBoxUpload.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxUpload.TabIndex = 9;
+            this.checkBoxUpload.Text = "uploadInfo";
+            this.checkBoxUpload.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 261);
+            this.Controls.Add(this.checkBoxUpload);
+            this.Controls.Add(this.checkBoxShuffle);
+            this.Controls.Add(this.checkBoxOutput);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listBoxSong);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ListBox listBoxSong;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.CheckBox checkBoxOutput;
+        private System.Windows.Forms.CheckBox checkBoxShuffle;
+        private System.Windows.Forms.CheckBox checkBoxUpload;
+    }
+}
+
---
+++ Musicplayer/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Net;
+
+namespace Musicplayer
+{
+
+    public partial class Form1 : Form
+    {
+        WMPLib.IWMPMedia[] wMedia;
+        public Form1()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(axWindowsMediaPlayer1_PlayStateChange);
+        }
+
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");
+            WMPLib.IWMPMedia media;
+            listBoxSong.Items.Clear();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Boolean mp3 = true;
+                Boolean m3u = false;
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    if(fi.Extension!=".mp3"&&fi.Extension!=".wav")
+                    {
+                        mp3 = false;
+                    }
+                    if(fi.Extension==".m3u")
+                    {
+                        m3u = true;
+                    }
+                }
+                if(mp3)
+                {
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        media = axWindowsMediaPlayer1.newMedia(file);
+                        playlist.appendItem(media);
+                        TagLib.File mediaNow = TagLib.File.Create(file);
+                        if (mediaNow.Tag.Performers.Length > 0 && mediaNow.Tag.Title != null)
+                        {
+                            listBoxSong.Items.Add(mediaNow.Tag.Performers[0].ToString() + " - " + mediaNow.Tag.Title.ToString());
+                        }
+                        else
+                        {
+                            listBoxSong.Items.Add(media.name);
+                        }
+                    }
+                }
+                if(m3u)
+                {
+                    playlist = axWindowsMediaPlayer1.newPlaylist("myPlaylist",openFileDialog.FileNames[0]);
+                    for(int i=0;i<playlist.count;i++)
+                    {
+                        TagLib.File mediaNow = TagLib.File.Create(playlist.Item[i].sourceURL);
+                        if (mediaNow.Tag.Performers.Length > 0 && mediaNow.Tag.Title != null)
+                        {
+                            listBoxSong.Items.Add(mediaNow.Tag.Performers[0].ToString() + " - " + mediaNow.Tag.Title.ToString());
+                        }
+                        else
+                        {
+                            listBoxSong.Items.Add(playlist.Item[i].name);
+                        }
+                    }
+                }
+            }
+            axWindowsMediaPlayer1.currentPlaylist = playlist;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            if(axWindowsMediaPlayer1.currentMedia!=null)
+            {
+                listBoxSong.SelectedItem = axWindowsMediaPlayer1.currentMedia.name;
+            }
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+        }
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if(axWindowsMediaPlayer1.currentMedia!=null)
+            {
+                TagLib.File mediaNow = TagLib.File.Create(axWindowsMediaPlayer1.currentMedia.sourceURL);
+                if (mediaNow.Tag.Performers.Length>0&&mediaNow.Tag.Title!=null)
+                {
+                    listBoxSong.SelectedItem = (mediaNow.Tag.Performers[0].ToString() + " - " + mediaNow.Tag.Title.ToString());
+                    this.Text = mediaNow.Tag.Performers[0].ToString() + " - " + mediaNow.Tag.Title.ToString();
+                }
+                else{
+                    this.Text = axWindowsMediaPlayer1.currentMedia.name;
+                    listBoxSong.SelectedItem = axWindowsMediaPlayer1.currentMedia.name;
+                }
+                if (mediaNow.Tag.Pictures.Length > 0)
+                {
+                    MemoryStream ms = new MemoryStream(mediaNow.Tag.Pictures[0].Data.Data);
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+                    Bitmap bp = new Bitmap(image);
+                    int a, r, g, b;
+                    a = 0; r = 0; g = 0; b = 0;
+                    for (int y = 1; y < bp.Height; y++)
+                    {
+                        for (int x = 1; x < bp.Width; x++)
+                        {
+                            a += bp.GetPixel(x, y).A;
+                            r += bp.GetPixel(x, y).R;
+                            g += bp.GetPixel(x, y).G;
+                            b += bp.GetPixel(x, y).B;
+                        }
+                    }
+                    int cc = bp.Height * bp.Width;
+                    Color cl = Color.FromArgb(255 ,r / cc, g / cc, b / cc);
+                    this.BackColor = cl;
+                }else
+                {
+                    this.BackColor = Color.AliceBlue;
+                }
+                if(checkBoxOutput.Checked)
+                {
+                    if(File.Exists("output.html"))
+                    {
+                        File.Delete("output.html");
+                    }
+                    if(File.Exists("output.txt"))
+                    {
+                        File.Delete("output.txt");
+                    }
+                    string color = "#A7FFF0";
+                    if (mediaNow.Tag.Pictures.Length > 0)
+                    {
+                        MemoryStream ms = new MemoryStream(mediaNow.Tag.Pictures[0].Data.Data);
+                        System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+                        image.Save("output.jpg");
+                        Bitmap bp = new Bitmap(image);
+                        int a, r, g, b;
+                        a = 0;r = 0;g = 0;b = 0;
+                        for(int y=1; y<bp.Height;y++)
+                        {
+                            for(int x=1;x<bp.Width;x++)
+                            {
+                                a+=bp.GetPixel(x, y).A;
+                                r += bp.GetPixel(x, y).R;
+                                g += bp.GetPixel(x, y).G;
+                                b += bp.GetPixel(x, y).B;
+                            }
+                        }
+                        int cc = bp.Height * bp.Width;
+                        Color cl=Color.FromArgb(a / cc, r / cc, g / cc, b / cc);
+                        color = ColorTranslator.ToHtml(cl).ToString();
+                        if(checkBoxUpload.Checked)
+                        {
+                            uploadImageToServer("output.jpg", "output.jpg");
+                        }
+                    }else
+                    {
+                        Image img = Image.FromFile("notfound.png");
+                        img.Save("output.jpg");
+
+                    }
+                    using (StreamWriter sw = File.CreateText("output.html"))
+                    {
+                        sw.WriteLine("<html>");
+                        sw.WriteLine("<head>");
+                        sw.WriteLine("<meta charset='unicode' http-equiv='cache-control' content='no-cache'>");
+                        sw.WriteLine("<link rel='stylesheet' href='stylesheet.css'>");
+                        //sw.WriteLine("<script src=\"jquery-1.12.3.js\"></script>");
+                        //sw.WriteLine("<script type='text/javascript'>"); sw.WriteLine("function refresh()");sw.WriteLine("{	setTimeout(function(){ $.ajax({");sw.WriteLine("    url : \"http://trivernis.byethost7.com/Music/index.html? \" + (new Date()).getMilliseconds(),");
+                        //sw.WriteLine("dataType : \"text\",");sw.WriteLine("    ifModified : true,");sw.WriteLine("    success : function(data, textStatus) {");sw.WriteLine("      if (textStatus != \"notmodified\") {");sw.WriteLine("        location.href = location.href;");
+                        //sw.WriteLine("}}});}, 1000);}");
+                        //sw.WriteLine("</script>");
+                        sw.WriteLine("</head>");
+                        sw.WriteLine("<body style='background-color:"+color+"' onLoad='refresh()'>");
+                        if(mediaNow.Tag.Pictures.Length > 0)
+                        {
+                            sw.WriteLine("<img class='art' src='output.jpg'/>");
+                        }else
+                        {
+                            sw.WriteLine("<img class='art' src='" + this.searchForImage(axWindowsMediaPlayer1.currentMedia.name) + "'/>");
+                        }
+                        if(mediaNow.Tag.Performers.Length > 0 && mediaNow.Tag.Title != null&&mediaNow.Tag.Album!=null)
+                        {
+                            sw.WriteLine("<p class='artist'>"+mediaNow.Tag.Performers[0]+"</p>");
+                            sw.WriteLine("<p class='title'>"+mediaNow.Tag.Title+"</p>");
+                            sw.WriteLine("<p class='album'>"+mediaNow.Tag.Album+"</p>");
+                        }
+                        else
+                        {
+                            sw.WriteLine("<p class='artist'>"+axWindowsMediaPlayer1.currentMedia.name+"</p>");
+                        }
+                        sw.WriteLine("</body>");
+                        sw.WriteLine("</html>");
+
+                    }
+                    if(checkBoxUpload.Checked)
+                    {
+                        uploadToServer("output.html", "index.html");
+                    }
+                    FileInfo fih = new FileInfo("output.html");
+                    WebsiteToImage websiteToImage = new WebsiteToImage("file:///"+fih.FullName,"weboutput.jpg");
+                    websiteToImage.Generate();
+                    using (StreamWriter sw = File.CreateText("background.html"))
+                    {
+                        sw.WriteLine("<html>");
+                        sw.WriteLine("<body style=\"background-color:"+color+"\">");
+                        sw.WriteLine("</body>");
+                    }
+                    FileInfo fib = new FileInfo("background.html");
+                    WebsiteToImage websitetoImage = new WebsiteToImage("file:///" + fib.FullName, "background.jpg");
+                    websitetoImage.Generate();
+                    using (StreamWriter sw = File.CreateText("output.txt"))
+                    {
+                        if (mediaNow.Tag.Performers.Length > 0 && mediaNow.Tag.Title != null && mediaNow.Tag.Album != null)
+                        {
+                            sw.WriteLine(mediaNow.Tag.Performers[0]);
+                            sw.WriteLine(mediaNow.Tag.Title);
+                            sw.WriteLine(mediaNow.Tag.Album);
+                        }
+                        else
+                        {
+                            sw.WriteLine(axWindowsMediaPlayer1.currentMedia.name);
+                        }
+                    }
+                }
+            }
+        }
+        private void listBoxSong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void checkBoxShuffle_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxShuffle.Checked)
+            {
+                axWindowsMediaPlayer1.settings.setMode("shuffle", true);
+            }else
+            {
+                axWindowsMediaPlayer1.settings.setMode("shuffle", false);
+            }
+        }
+        private void uploadToServer(string filename, string serverfile)
+        {
+            deleteFromServer(serverfile);
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://b7_18021240@ftp.byethost7.com/htdocs/Music/"+serverfile);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("b7_18021240", "Findus1608");
+
+            // Copy the contents of the file to the request stream.
+            StreamReader sourceStream = new StreamReader(filename,Encoding.Unicode);
+            byte[] fileContents = Encoding.Unicode.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+
+            response.Close();
+        }
+        private void uploadImageToServer(string Image, string serverfile)
+        {
+            deleteFromServer(serverfile);
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://b7_18021240@ftp.byethost7.com/htdocs/Music/" + serverfile);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("b7_18021240", "Findus1608");
+
+            // Copy the contents of the file to the request stream.
+            byte[] fileContents = File.ReadAllBytes(Image);
+            request.ContentLength = fileContents.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+
+            response.Close();
+        }
+        private void deleteFromServer(string filename)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://b7_18021240@ftp.byethost7.com/htdocs/Music/"+filename);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("b7_18021240", "Findus1608");
+        }
+        private string searchForImage(string searchstring)
+        {
+            Google.API.Search.GimageSearchClient ImageSearch = new Google.API.Search.GimageSearchClient("http://google.de");
+            try { return (ImageSearch.Search(searchstring, 1)[1].VisibleUrl); } catch
+            {
+                return ("notfound.png");
+            }
+        }
+    }
+}
